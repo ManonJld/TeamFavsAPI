c5e549df693995867d9860aa2102ea65e28557d4
Manon <manon.jeuland@gmail.com>
2021-04-18 11:43:40 +0200

    cancel last commit

--- src/Doctrine/CurrentUserExtension.php
+++ src/Doctrine/CurrentUserExtension.php
@@ -24,7 +24,6 @@ class CurrentUserExtension implements QueryCollectionExtensionInterface
     }
 
 
-
     public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
     {
         //Obtenir l'utilisateur connecté
